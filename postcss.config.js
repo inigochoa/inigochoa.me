d08577b8b477a13759b2ae541a4fd19014b3c3fd
Iñigo Ochoa <contact@inigochoa.me>
2021-03-20 11:24:32 +0100

    [css] add Tailwind CSS JIT to boost build speed

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ const cssnano = require('cssnano')({
 module.exports = {
   plugins: [
     require('postcss-nested'),
-    require('tailwindcss'),
+    require('@tailwindcss/jit'),
     require('autoprefixer'),
     ...process.env.NODE_ENV == 'production'
       ? [cssnano]
