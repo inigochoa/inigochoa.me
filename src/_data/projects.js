07566f2a59252c3ddf3e21ad0cf48ab426e91ce0
Iñigo Ochoa <contact@inigochoa.me>
2021-04-13 08:29:32 +0200

    feat: add monitorbot-statuspage project

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -1,5 +1,11 @@
 module.exports = [
   {
+    desc: 'Status page for Monitorbot. Real-time updates for the status of your systems.',
+    lang: ['Vue', 'JavaScript', 'HTML', 'CSS'],
+    link: 'https://github.com/inigochoa/monitorbot-statuspage',
+    name: 'monitorbot-statuspage',
+  },
+  {
     desc: 'A private Telegram bot for web page monitoring. Add a list of URLs and be notified when their status changes.',
     lang: ['JavaScript'],
     link: 'https://github.com/inigochoa/monitorbot',
