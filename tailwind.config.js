cb01e62b5d7efdff007ad1c903e73fea028f75b1
Iñigo Ochoa <contact@inigochoa.me>
2021-07-07 18:45:00 +0200

    style: added new light colors set

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,11 +13,48 @@ module.exports = {
     extend: {
       colors: {
         garnet: 'rgba(255, 0, 0, 0.6)',
+        light: {
+          arrow: '#9ca3af',
+          background: '#f3f4f6',
+          card: '#ffffff',
+          content: '#ffffff',
+          primary: '#000000',
+          secondary: '#374151',
+          tertiary: '#6b7280',
+        },
       },
       typography: theme => ({
         DEFAULT: {
           css: [
             {
+              color: theme('colors.light.secondary'),
+              'h1': {
+                color: theme('colors.light.primary'),
+              },
+              'h2': {
+                color: theme('colors.light.primary'),
+              },
+              'h3': {
+                color: theme('colors.light.primary'),
+              },
+              'h4': {
+                color: theme('colors.light.primary'),
+              },
+              'h5': {
+                color: theme('colors.light.primary'),
+              },
+              'h6': {
+                color: theme('colors.light.primary'),
+              },
+              a: {
+                color: theme('colors.light.secondary'),
+              },
+              code: {
+                color: theme('colors.light.primary'),
+              },
+              strong: {
+                color: theme('colors.light.primary'),
+              },
               'h1::before': {
                 color: theme('colors.garnet'),
                 content: '"#"',
